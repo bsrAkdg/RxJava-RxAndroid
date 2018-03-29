01f553fd9becc7ff22d21e195ac2f486652851ab
Büşra Serdaroğlu <bakdag@mngkargo.com.tr>
2018-03-29 16:05:48 +0300

    RxView ve Buffer operator kullanılarak rxjava denemesi

---
+++ BufferOperatorApp/app/src/main/java/com/buffer/bakdag/bufferoperatorapp/MainActivity.java
@@ -0,0 +1,88 @@
+package com.buffer.bakdag.bufferoperatorapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.jakewharton.rxbinding2.view.RxView;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+
+
+public class MainActivity extends AppCompatActivity {
+    // 3 saniye içerisinde kaç kere tıklanıldığını gösteren ve maksimum tıklanma sayısını gösteren app.
+    @BindView(R.id.tap_result)
+    TextView txtTapResult;
+
+    @BindView(R.id.tap_result_max_count)
+    TextView txtTapResultMax;
+
+    @BindView(R.id.layout_tap_area)
+    Button btnTapArea;
+
+    private Disposable disposable;
+    private Unbinder unbinder;
+    private int maxTaps = 0;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        unbinder = ButterKnife.bind(this);
+
+        RxView.clicks(btnTapArea)
+                .map(new Function<Object, Integer>() {
+                    @Override
+                    public Integer apply(Object o) throws Exception {
+                        return 1;
+                    }
+                })
+                .buffer(5, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new Observer<List<Integer>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable = d;
+                    }
+
+                    @Override
+                    public void onNext(List<Integer> integers) {
+                        Log.e(TAG, "onNext: " + integers.size() + " kere tıklandı!");
+                        if (integers.size() > 0) {
+                            maxTaps = integers.size() > maxTaps ? integers.size() : maxTaps;
+                            txtTapResult.setText(String.format("3 sn içinde tıklama sayınız : %d", integers.size()));
+                            txtTapResultMax.setText(String.format("Max tıklanma sayısı : %d" , maxTaps));
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "onComplete");
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+        disposable.dispose();
+    }
+}
